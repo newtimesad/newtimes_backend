c44171d60628130d55874cc00b3eef199e16d3fc
daironigr <daironigr@gmail.com>
2021-08-24 20:58:10 -0400

    init

--- common/config/params.php
+++ common/config/params.php
@@ -6,6 +6,6 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
-    'appName' => 'Colero',
+    'appName' => 'Yii2AdvancedApplication',
     'defaultLanguage' => 'es'
 ];
