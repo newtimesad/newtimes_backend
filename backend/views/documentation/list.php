237262b08379f183620997e92995f7d0b06f9e71
Dairon Ian Garcia Roque <39626540+daironigr@users.noreply.github.com>
2022-03-04 08:02:46 +0000

    - fixes

--- backend/views/documentation/list.php
+++ backend/views/documentation/list.php
@@ -8,6 +8,7 @@ use yii\bootstrap4\BootstrapAsset;
 use yii\bootstrap4\Html;
 use yii\web\View;
 
+$this->title = Yii::t('app', "Legal Documentation");
 BootstrapAsset::register($this);
 $attributes = $documentation->getAttributes(null, ['id']);
 
